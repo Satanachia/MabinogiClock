951f0b7698b069eb005041b3996d8b1ba2b59b57
Snowy <snowyyang@outlook.com>
2021-08-21 21:10:56 +0800

    first draft

---
+++ Clock.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MabinogiClock
+{
+    class Clock : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        bool _isEnabled;
+        public bool IsEnabled
+        {
+            get => _isEnabled;
+            set
+            {
+                _isEnabled = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("IsEnabled"));
+            }
+        }
+        public DateTime MabinogiTime { get; private set; }
+        string _timeText;
+        public string TimeText
+        {
+            get => _timeText;
+            set
+            {
+                _timeText = value;
+                try
+                {
+                    var t = value.Split(':','：');
+                    MabinogiTime = new DateTime(2021, 1, 1, int.Parse(t[0]), int.Parse(t[1]), 0);
+                    IsInvalid = false;
+                }
+                catch
+                {
+                    IsInvalid = true;
+                }
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(null));
+            }
+        }
+        public bool IsInvalid { get; set; }
+
+        public static DateTime Real2Mabinogi(DateTime realTime)
+        {
+            //爱琳时间24小时=现实36分钟，60分钟=90秒，1分钟=1.5秒
+            var seconds = (realTime.Hour * 60 + realTime.Minute)*60 + realTime.Second;
+            var mabiMinutes = (int)(seconds / 1.5);
+            var mabiHour = (mabiMinutes / 60) % 24;
+            var mabiMinute = mabiMinutes % 60;
+            return new DateTime(2021, 1, 1, mabiHour, mabiMinute, 0);
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace MabinogiClock
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SoundPlayer sound;
+        DispatcherTimer timer;
+        ObservableCollection<Clock> _clocks = new ObservableCollection<Clock>() { new Clock() { TimeText = "19:00" } };
+        public MainWindow()
+        {
+            InitializeComponent();
+            clocks.ItemsSource = _clocks;
+            timer = new DispatcherTimer() { Interval = TimeSpan.FromSeconds(1.5) };
+            timer.Tick += Timer_Tick;
+            timer.Start();
+            sound = new SoundPlayer("save.wav");
+            sound.Load();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            var now = Clock.Real2Mabinogi(DateTime.Now);
+            this.now.Text = now.ToString("HH:mm");
+            foreach(var c in _clocks)
+                if (c.IsEnabled && !c.IsInvalid && c.MabinogiTime.Hour == now.Hour && c.MabinogiTime.Minute == now.Minute)
+                {
+                    MessageBox.Show(this.now.Text, Title, MessageBoxButton.OK, MessageBoxImage.None, MessageBoxResult.None, MessageBoxOptions.ServiceNotification);
+                    sound.Play();
+                }
+        }
+
+        private void NewMabinogiClock_Click(object sender, RoutedEventArgs e)
+        {
+            _clocks.Add(new Clock() { IsEnabled = true, TimeText = now.Text });
+        }
+    }
+}
